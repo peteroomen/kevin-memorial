63c599e92584ba04bec693a8e3ab7fdd9a885d3b
Peter Oomen <peter.oomen@ambitai.com>
2022-12-13 17:18:51 +1300

    updated port

--- kevin-memorial/app.js
+++ kevin-memorial/app.js
@@ -10,7 +10,7 @@ app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-const port = 9000;
+const port = 80;
 app.listen(port);
 
 console.log(`Listening on port ${port}`)
